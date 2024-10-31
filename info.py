7f4c1cefe33e0a06359ac5cac441f7696617a956
YuvrajSharwa88 <yuvrajsarva6@gmail.com>
2024-11-01 00:12:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://vayimo1960
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '-1002384958285') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_group77')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_provider_group4')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+x7DVuvgQ6yE5ODJl')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
